1fa24e18e3cc9d995ab3b1da1b68ced258f44071
JoaoPedro-Nascente <64172268+JoaoPedro-Nascente@users.noreply.github.com>
2025-12-01 23:09:45 -0300

    feat: adiciona o model principal (Transaction)

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MinValueValidator
+import uuid
 
-# Create your models here.
+class TypeTransaction(models.TextChoices):
+    """
+    Classe auxiliar de Transaction.transaction_type
+    """
+    INCOME = "income", "Income Transaction"
+    OUTCOME = "outcome", "Outcome Transaction"
+
+class Transaction(models.Model):
+    """
+    Modelo principal
+    """
+    transaction_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    transaction_description = models.CharField(
+        max_length=100,
+        default=""
+    )
+
+    transaction_amount = models.DecimalField(
+        default=0.0,
+        max_digits=12,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.0)
+        ]
+    )
+    
+    transaction_type = models.CharField(
+        max_length=50,
+        choices=TypeTransaction.choices
+    )
+    
+    transaction_date = models.DateField(
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return super().__str__()
--- api_root/settings.py
+++ api_root/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api_rest',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api_root.urls'
@@ -120,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# TODO: Rever antes de entregar
+#CORS_ALLOW_ORIGINS_ALL = True
+
+CORS_ALLOW_ORIGINS = [
+    'http://localhost:8080',
+]
