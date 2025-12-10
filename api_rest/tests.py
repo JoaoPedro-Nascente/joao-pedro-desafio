66c06dc13b3182d6668d188269f57c651c4606c7
JoaoPedro-Nascente <64172268+JoaoPedro-Nascente@users.noreply.github.com>
2025-12-10 20:10:02 -0300

    test: added tests for auth

--- api_rest/tests.py
+++ api_rest/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 from rest_framework import status
 from decimal import Decimal
@@ -44,6 +45,41 @@ class HelpersTestCase(TestCase):
         self.assertEqual(filtered_transactions.count(), 1)
         self.assertEqual(filtered_transactions.first().description.lower(), "aluguel")
 
+#Authentication Unit Tests
+class AuthAPITestCase(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('user_register')
+        self.login_url = reverse('token_obtain_pair')
+        self.username = 'testUser'
+        self.password = 'testPassword'
+        self.valid_data = {
+            'username': self.username,
+            'password': self.password
+        }
+
+    def test_user_registration_success(self):
+        innitial_count = User.objects.count()
+        response = self.client.post(self.register_url, self.valid_data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), innitial_count + 1)
+        self.assertIn('username', response.data)
+
+    def test_user_registration_duplicate(self):
+        self.client.post(self.register_url, self.valid_data, format='json')
+        response = self.client.post(self.register_url, self.valid_data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('username', response.data)
+
+    def test_token_obtain_success(self):
+        self.client.post(self.register_url, self.valid_data, format='json')
+
+        response = self.client.post(self.login_url, self.valid_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
 
 #Views unit tests
 class TransactionAPITestCase(APITestCase):
